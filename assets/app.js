043ebccbc6d7658d427259c2882da43eab445ac4
Chris Livingston <clivingston@gmail.com>
2021-08-15 18:31:56 -0700

    Splash Page Deployed

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+// 1. You will only have <span>15 seconds</span> per each question.
+// 2. Once you select and answer, you cannot reselect.
+// 3. You can't select any option once timer goes off.
+// 4. You can't exit from the quiz while you are playing.
+// 5.You'll get points on the basis of your correct answers.
+
+//  What is the special character call out for a class in CSS?
+// $
+// !
+// &
+// . 
+
+// What does CSS stand for?
+// Cool Style Syntax
+// Cause Smooth Style
+// Come Super Saying
+// Cascading Style Sheet
+
+//What does HTML stand for?
+// Hot Tamali Mama Love
+// Hyper Text MarkUp Language
+// Hyper Tour Masking Light
+// Have Too Much Load
+
+// What is Repo short for?
+// Grim Reaper
+// Repossesion
+// Repository
+// Nothing
+
